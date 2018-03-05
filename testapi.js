0e544c2f89557d9cf79d8978455a474d4734ac2d
Frank Dominguez <frankd1997@gmail.com>
2018-03-05 17:45:16 -0600

    commit

--- testapi.js
+++ testapi.js
@@ -2,11 +2,10 @@
 var songs = ["Strawberry Fields Forever", "Blackbird", "Hey, Jude", "When You Were Young"];
 
   var song = "Strawberry Fields Forever";
-  var queryURL = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao&apikey=ed7872ae8c7eb0045a2017ba672f09a8";
+  var queryURL = "https://api.musixmatch.com/ws/1.1/track.search?q_track="+ song +"&apikey=ed7872ae8c7eb0045a2017ba672f09a8";
   
 
   // Creating an AJAX call for the specific movie button being clicked
-  
   $.ajax({
     url: queryURL,
     method: "GET",

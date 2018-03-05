71d0983fef4e68f4bd47b7ae0d0577cb2f569573
Frank Dominguez <frankd1997@gmail.com>
2018-03-05 16:49:35 -0600

    commit

--- testapi.js
+++ testapi.js
@@ -2,7 +2,7 @@
 var songs = ["Strawberry Fields Forever", "Blackbird", "Hey, Jude", "When You Were Young"];
 
   var song = $(this).attr("data-name");
-  var queryURL = "https://api.musixmatch.com/ws/1.1/track.search?q_track="+ song +"&apikey=ed7872ae8c7eb0045a2017ba672f09a8";
+  var queryURL = "https://api.musixmatch.com/ws/1.1/track.search?q_track=revolution%9&apikey=ed7872ae8c7eb0045a2017ba672f09a8";
   
 
   // Creating an AJAX call for the specific movie button being clicked
